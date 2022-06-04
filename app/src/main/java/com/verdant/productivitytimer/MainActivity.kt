7e1e646cade2f3b6e2735f6a242134d63e68c05d
JaceVerdant <jaceverdant@gmail.com>
2022-06-04 22:59:55 +0200

    add ui

--- app/src/main/java/com/verdant/productivitytimer/MainActivity.kt
+++ app/src/main/java/com/verdant/productivitytimer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.verdant.productivitytimer
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timer: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        timer = findViewById(R.id.timer)
+
+        timer.setOnClickListener {
+            AlertDialog.Builder(this, R.style.Theme_ProductivityTimer)
+                .setTitle("Set timer")
+                .setView(LayoutInflater.from(this).inflate(R.layout.picker, null, false))
+                .setPositiveButton("Confirm") { _, _ ->
+                    Toast.makeText(this, "Ok", Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton(android.R.string.cancel, null)
+                .show()
+        }
     }
 }
